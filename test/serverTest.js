652016e29d2c76aec8c43197a6efc66c9f5577f6
Kevin98-x <kevinmuthama8@gmail.com>
2024-11-22 04:55:27 +0300

    2

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,26 +1,39 @@
+// Set the environment to 'test' for proper configuration during testing
 process.env.NODE_ENV = 'test';   
 
+// Import necessary modules
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 
+// Import the server instance
 var server = require('../server');
 var should = chai.should();
-var expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Photos', function(){
+// Test Suite: Photos
+describe('Photos', function() {
 
+    // Test Case: List all photos
+    it('should list ALL photos on / GET', function(done) {
+        this.timeout(60000); // Set timeout to 60 seconds to allow sufficient response time
+        
+        chai.request(server) // Make a request to the server
+        .get('/') // Target the root route
+        .end(function(err, res) {
+            // Debugging logs for troubleshooting
+            if (err) {
+                console.error('Error occurred:', err);
+            }
+            console.log('Response Status:', res.status);
+            console.log('Response Body:', res.body);
 
-    it('should list ALL photos on / GET', function(done){
-        this.timeout(60000);
-        chai.request(server)
-        .get('/')
-        .end(function(err,res){
-            res.should.have.status(200);
-            res.should.be.html;
-            res.body.should.be.a('object')
-            done();
-        })
+            // Assertions
+            res.should.have.status(200); // Assert that the response status is 200
+            res.should.be.json;         // Assert that the response is in JSON format
+            res.body.should.be.a('array'); // Assert that the response body is an array
+            
+            done(); // Indicate that the test is complete
+        });
     });
-})
+});
